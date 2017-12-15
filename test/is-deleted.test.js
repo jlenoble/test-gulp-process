7633cf40c34afcd3966893b3501fb2546937e406
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-15 20:26:26 +0100

    isDeleted does nothing on bad glob

--- test/is-deleted.test.js
+++ test/is-deleted.test.js
@@ -53,4 +53,27 @@ describe('Testing Gulpfile', function () {
       expect(err.message).to.match(/resolves to nothing/);
     },
   }));
+
+  it(`Testing isDeleted(badGlob)`, testGulpProcess({
+    sources: ['src/**/*.js', 'test/**/*.js', 'gulp/**/*.js'],
+    gulpfile: 'test/gulpfiles/tdd-transpile-all.js',
+    debug: true,
+
+    messages: [
+      `Starting 'default'...`,
+      `Starting 'tdd:transpile:all'...`,
+      `Starting 'exec:transpile:all'...`,
+      `Finished 'exec:transpile:all' after`,
+      `Starting 'watch:transpile:all'...`,
+      `Finished 'watch:transpile:all' after`,
+      `Finished 'tdd:transpile:all' after`,
+      [`Finished 'default' after`,
+        deleteFile('src/test-tools/*.js'),
+        deleteFile('build/src/test-tools/*.js')],
+      `Starting 'exec:transpile:all'...`,
+      [`Finished 'exec:transpile:all' after`,
+        isDeleted('src/test-tools/*.js'),
+        isDeleted('badbuild/src/test-tools/*.js')],
+    ],
+  }));
 });

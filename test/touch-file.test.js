8a74264b71be4c6b05ad91d7736aca0998b3d059
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-15 21:09:57 +0100

    touchFile throws on bad glob

--- test/touch-file.test.js
+++ test/touch-file.test.js
@@ -1,4 +1,5 @@
 import testGulpProcess, {touchFile} from '../src/test-gulp-process';
+import {expect} from 'chai';
 
 describe('Testing Gulpfile', function () {
   it(`Testing touchFile(glob)`, testGulpProcess({
@@ -19,5 +20,29 @@ describe('Testing Gulpfile', function () {
       `Starting 'exec:transpile:all'...`,
       `Finished 'exec:transpile:all' after`,
     ],
+  }));
+
+  it(`Testing touchFile(badglob)`, testGulpProcess({
+    sources: ['src/**/*.js', 'test/**/*.js', 'gulp/**/*.js'],
+    gulpfile: 'test/gulpfiles/tdd-transpile-all.js',
+    fullDebug: true,
+
+    messages: [
+      `Starting 'default'...`,
+      `Starting 'tdd:transpile:all'...`,
+      `Starting 'exec:transpile:all'...`,
+      `Finished 'exec:transpile:all' after`,
+      `Starting 'watch:transpile:all'...`,
+      `Finished 'watch:transpile:all' after`,
+      `Finished 'tdd:transpile:all' after`,
+      [`Finished 'default' after`,
+        touchFile('badsrc/test-tools/*.js')],
+      `Starting 'exec:transpile:all'...`,
+      `Finished 'exec:transpile:all' after`,
+    ],
+
+    onCheckResultsError (err) {
+      expect(err.message).to.match(/resolves to nothing/);
+    },
   }));
 });

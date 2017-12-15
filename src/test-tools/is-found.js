b98631f6cc99ad0c9f7fbb43dab32b854e0c7a31
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-15 18:00:49 +0100

    isFound can accept a glob

--- src/test-tools/is-found.js
+++ src/test-tools/is-found.js
@@ -1,12 +1,14 @@
-import {rebaseGlob} from 'polypath';
+import {resolveGlob, rebaseGlob} from 'polypath';
 import chalk from 'chalk';
 import {expectEventuallyFound} from 'stat-again';
 
 export const isFound = _file => options => {
-  const [file] = rebaseGlob(_file, options.dest);
-  if (options && options.debug) {
-    console.info(`${chalk.cyan('Checking')} whether ${
-      chalk.green(file)} can be found`);
-  }
-  return expectEventuallyFound(file);
+  return resolveGlob(rebaseGlob(_file, options.dest)).then(files => Promise.all(
+    files.map(file => {
+      if (options && options.debug) {
+        console.info(`${chalk.cyan('Checking')} whether ${
+          chalk.green(file)} can be found`);
+      }
+      return expectEventuallyFound(file);
+    })));
 };

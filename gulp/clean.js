4c792946bf20a8c9a695f6217613d61fd9cc1dd0
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-14 12:03:12 +0200

    Removed regeneratable files

--- gulp/clean.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import del from 'del';
-
-export const clean = () => {
-  return del('build');
-};
-
-gulp.task('clean', clean);
--- gulp/prepublish.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-
-import './test';
-import './distclean';
-import './dist';
-import './doc';
-
-gulp.task('prepublish', gulp.series('test', 'distclean', 'dist', 'doc'));

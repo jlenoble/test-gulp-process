dbe4b230aed283004eb39097e4787b6f34cac86a
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-15 16:26:13 +0200

    added typings

--- src/test-tools/compared-transpiled.ts
+++ src/test-tools/compared-transpiled.ts
@@ -3,8 +3,12 @@ import chalk from "chalk";
 import path from "path";
 import equalFileContents from "equal-file-contents";
 import babel from "gulp-babel";
+import { DestFn, DestOptions } from "./options";
 
-export const compareTranspiled = (_glob, _dest) => options => {
+export const compareTranspiled = (
+  _glob: string | string[],
+  _dest: string
+): DestFn => (options: DestOptions): Promise<boolean> => {
   const dest = path.join(options.dest, _dest);
   const glob = rebaseGlob(_glob, options.dest);
   if (options && options.debug) {

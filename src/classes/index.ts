605557bc9ab69c4fcde0a7267e21897f56fd7e50
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-15 12:54:44 +0200

    export types from task-messages

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,4 +1,8 @@
 export { default as File } from "./file";
 export { default as Messages } from "./messages";
 export { default as ParallelMessages } from "./parallel-messages";
-export { default as TaskMessages } from "./task-messages";
+export {
+  default as TaskMessages,
+  TaskMessagesArray,
+  TaskMessagesOptions
+} from "./task-messages";

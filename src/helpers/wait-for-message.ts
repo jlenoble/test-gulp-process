9e20e7e7c6864a58395d0f1b321919d4167761a9
Jason Lenoble <jason.lenoble@gmail.com>
2019-07-15 12:20:58 +0200

    Fixed allMessages
    Result Api was changed from accessor to func
    
    Also added typings

--- src/helpers/wait-for-message.ts
+++ src/helpers/wait-for-message.ts
@@ -1,14 +1,22 @@
-const repeat = (action, interval = 200, maxDuration = 4000) => {
-  let intervalId;
-  let timeoutId;
+import { Result } from "child-process-data";
 
-  return new Promise((resolve, reject) => {
-    const timeout = () => {
+const repeat = (
+  action,
+  interval = 200,
+  maxDuration = 4000
+): Promise<boolean> => {
+  return new Promise((resolve, reject): void => {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    const intervalId = setInterval(autoCleanAction, interval);
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    const timeoutId = setTimeout(timeout, maxDuration);
+
+    function timeout(): void {
       clearInterval(intervalId);
       reject(new Error("Waiting too long for child process to finish"));
-    };
+    }
 
-    const autoCleanAction = () => {
+    function autoCleanAction(): void {
       try {
         if (action()) {
           clearTimeout(timeoutId);
@@ -20,28 +28,33 @@ const repeat = (action, interval = 200, maxDuration = 4000) => {
         clearInterval(intervalId);
         reject(e);
       }
-    };
-
-    intervalId = setInterval(autoCleanAction, interval);
-    timeoutId = setTimeout(timeout, maxDuration);
+    }
 
     autoCleanAction(); // Start immediately
   });
 };
 
-const testMessage = (results, message) => {
+const testMessage = (results: Result, message: string): boolean => {
   return (
-    results.allMessages.findIndex(el => el.match(new RegExp(message))) !== -1
+    results
+      .allMessages()
+      .findIndex((el): boolean => !!el.match(new RegExp(message))) !== -1
   );
 };
 
-export function waitForMessage(results, message) {
+export async function waitForMessage(
+  results: Result,
+  message: string
+): Promise<boolean> {
   const msg = message.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-  return repeat(() => testMessage(results, msg)).catch(err => {
+
+  try {
+    return repeat((): boolean => testMessage(results, msg));
+  } catch (err) {
     if (err.message.match(/Waiting too long for child process to finish/)) {
       throw new Error(`Waiting too long for child process to finish:
 Message '${message}' was never intercepted`);
     }
     throw err;
-  });
+  }
 }
